17cceeb5152ffc23971c1c5b638ef80da59f8c7c
Iftekar Islam <iftekharislam923@gmail.com>
2026-07-02 21:50:48 +0600

    first commit

---
+++ binary_tree.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+class Node{
+    public:
+       int val;
+       Node* left;
+       Node* right;
+    Node(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+      
+    
+      
+    return 0;
+}
